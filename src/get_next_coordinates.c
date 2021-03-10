7fa00fe36572b33400327320b94f05a5325764ad
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-03-10 08:24:57 +0200

    adjust coordinate checks to find all valid locations

--- src/get_next_coordinates.c
+++ src/get_next_coordinates.c
@@ -6,7 +6,7 @@
 /*   By: skoskine <skoskine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 22:39:58 by skoskine          #+#    #+#             */
-/*   Updated: 2021/03/09 22:20:39 by skoskine         ###   ########.fr       */
+/*   Updated: 2021/03/10 08:21:47 by skoskine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,8 @@ static int	valid_piece_coordinates(int y, int x, t_data data)
 		{
 			if (data.piece.map[i.y * data.piece.width + i.x] == '*')
 			{
-				if (y + i.y >= data.board.height || x + i.x >= data.board.width)
+				if (y + i.y >= data.board.height || x + i.x >= data.board.width ||
+				y + i.y < 0 || x + i.x < 0)
 					return (0);
 				cell = data.board.map[(y + i.y) * data.board.width + x + i.x];
 				if (ft_tolower(cell) == data.player_piece)
@@ -40,7 +41,7 @@ static int	valid_piece_coordinates(int y, int x, t_data data)
 		}
 		i.y++;
 	}
-	return (overlap == 1 ? 1 : 0);
+	return (overlap);
 }
 
 static void	write_coordinates(size_t y, size_t x)
@@ -54,20 +55,27 @@ static void	write_coordinates(size_t y, size_t x)
 void		get_next_coordinates(t_data data)
 {
 	t_2d_index	piece;
+	t_2d_index	piece_start;
 	t_2d_index	next_piece;
 //	t_2d_index	opponent_piece;
 
 	next_piece.y = 0;
 	next_piece.x = 0;
+	piece_start.y = 0;
+	while (row_is_empty(data.piece.map, piece_start.y, data.piece.width))
+		piece_start.y++;
+	piece_start.x = 0;
+	while (data.piece.map[piece_start.y * data.piece.width + piece_start.x] != '*')
+		piece_start.x++;
 	piece.y = 0;
 	while (piece.y < data.board.height)
 	{
 		piece.x = 0;
 		while (piece.x < data.board.width)
 		{
-			if (valid_piece_coordinates(piece.y, piece.x, data))
+			if (valid_piece_coordinates(piece.y - piece_start.y, piece.x - piece_start.x, data))
 			{
-				write_coordinates(piece.y, piece.x);
+				write_coordinates(piece.y - piece_start.y, piece.x - piece_start.x);
 				return ;
 			}
 			piece.x++;
