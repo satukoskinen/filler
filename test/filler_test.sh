06d6ecd85d08e983b1df8b326f886eb3287f1893
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-03-14 15:38:19 +0200

    filler test script

---
+++ test/filler_test.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+### CONFIG ###
+
+player="./skoskine.filler"
+#opponent_dir="resources/players/*"
+opponent_dir="./ksuomala.filler"
+map_dir="resources/maps/*"
+vm="resources/filler_vm_old"
+logfile="test_log"
+
+rm -f $logfile
+
+printf "testing $player...\n"
+
+## Test with maps provided in resources:
+
+# Multi-player: as p1 and p2 against all other players,
+# each repeated 10 times
+
+read -n 1 -p "test multiplayer? [y/n] " ret
+if [ $ret != "n" ]
+then
+	printf "\n\nmultiplayer:\n"
+	for map in $map_dir
+	do
+		printf "\nmap $map:\n"
+		for opponent in $opponent_dir
+		do
+		printf "\nagainst $opponent:"
+			p1=$player
+			p2=$opponent
+			printf "\nas p1: "
+			for i in {1..10}
+			do
+				./$vm -f $map -p1 $p1 -p2 $p2 > output
+				grep "Segfault" output > error
+				if [ -s error ]
+				then
+					cat error
+					printf "map $map p1 $p1 p2 $p2\n" >> $logfile
+					cat filler.trace >> $logfile
+				fi
+				grep "fin" output > result
+				player_result=$(grep "O" result | cut -d " " -f 4)
+				opponent_result=$(grep "X" result | cut -d " " -f 4)
+				if [ $player_result -gt $opponent_result ]
+				then
+					printf "1"
+				else
+					printf "."
+					printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
+					cat filler.trace >> $logfile
+				fi
+			done
+			p2=$player
+			p1=$opponent
+			printf "\nas p2: "
+			for i in {1..10}
+			do
+				./$vm -f $map -p1 $p1 -p2 $p2 > output
+				grep "Segfault" output > error
+				if [ -s error ]
+				then
+					cat error
+					printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
+					cat filler.trace >> $logfile
+				fi
+				grep "fin" output > result
+				player_result=$(grep "X" result | cut -d " " -f 4)
+				opponent_result=$(grep "O" result | cut -d " " -f 4)
+				if [ $player_result -gt $opponent_result ]
+				then
+					printf "1"
+				else
+					printf "."
+					printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
+					cat filler.trace >> $logfile
+				fi
+			done
+			printf "\n"
+		done
+	done
+fi
+
+# Single player: each map 10 times
+
+printf "\n"
+read -n 1 -p "test single player? [y/n] " ret
+if [ $ret != "n" ]
+then
+	printf "\n\nsingle player:\n"
+	for map in $map_dir
+	do
+		printf "\nmap $map:\n"
+		for i in {1..10}
+		do
+			./$vm -f $map -p1 $player > output
+			grep "Segfault" output > error
+			if [ -s error ]
+			then
+				printf "segfault\n"
+				printf "\n./$vm -f $map -p1 $player\n" >> $logfile
+				cat filler.trace >> $logfile
+			fi
+			grep "fin" output
+		done
+	done
+fi
+
+rm -f output result error
+
+# Generate maps of different sizes with random starting points,
+# otherwise as above
