87b4730e473452cfcdab7895e1713dde47f08c2b
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-03-17 11:23:18 +0200

    added function

--- test/filler_test.sh
+++ test/filler_test.sh
@@ -9,11 +9,53 @@ map_dir="resources/maps/*"
 vm="resources/filler_vm_old"
 logfile="test_log"
 
-rm -f $logfile
+##############
 
-printf "testing $player...\n"
+play () {
+	map=$1
+	p1=$2
+	p2=$3
+
+	for i in {1..10}
+	do
+		./$vm -f $map -p1 $p1 -p2 $p2 > output
+		grep "Segfault" output > error
+		if [ -s error ]
+		then
+			filename="log_${i}"
+			cat output > $filename
+			printf "\nsegfault: see $filename\n"
+			printf "map $map p1 $p1 p2 $p2\n" >> $logfile
+			cat filler.trace >> $logfile
+		fi
+		grep "fin" output > result
+		p1_result=$(grep "O" result | cut -d " " -f 4)
+		p2_result=$(grep "X" result | cut -d " " -f 4)
+		if [ $p1 == $player ]
+		then
+			player_result=$p1_result
+			opponent_result=$p2_result
+		else
+			player_result=$p2_result
+			opponent_result=$p1_result
+		fi
+		if [ $player_result -gt $opponent_result ]
+		then
+			printf "1"
+		else
+			printf "."
+			printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
+			cat filler.trace >> $logfile
+		fi
+	done
+}
+
+##############
 
 ## Test with maps provided in resources:
+
+rm -f $logfile
+printf "testing $player...\n"
 
 # Multi-player: as p1 and p2 against all other players,
 # each repeated 10 times
@@ -28,60 +70,10 @@ then
 		for opponent in $opponent_dir
 		do
 		printf "\nagainst $opponent:"
-			p1=$player
-			p2=$opponent
 			printf "\nas p1: "
-			for i in {1..10}
-			do
-				./$vm -f $map -p1 $p1 -p2 $p2 > output
-				grep "Segfault" output > error
-				if [ -s error ]
-				then
-					filename="log_${i}"
-					cat output > $filename
-					printf "\nsegfault: see $filename\n"
-					printf "map $map p1 $p1 p2 $p2\n" >> $logfile
-					cat filler.trace >> $logfile
-				fi
-				grep "fin" output > result
-				player_result=$(grep "O" result | cut -d " " -f 4)
-				opponent_result=$(grep "X" result | cut -d " " -f 4)
-				if [ $player_result -gt $opponent_result ]
-				then
-					printf "1"
-				else
-					printf "."
-					printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
-					cat filler.trace >> $logfile
-				fi
-			done
-			p2=$player
-			p1=$opponent
+			play $map $player $opponent
 			printf "\nas p2: "
-			for i in {1..10}
-			do
-				./$vm -f $map -p1 $p1 -p2 $p2 > output
-				grep "Segfault" output > error
-				if [ -s error ]
-				then
-					filename="log_${i}"
-					cat output > $filename
-					printf "\nsegfault: see $filename\n"
-					printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
-					cat filler.trace >> $logfile
-				fi
-				grep "fin" output > result
-				player_result=$(grep "X" result | cut -d " " -f 4)
-				opponent_result=$(grep "O" result | cut -d " " -f 4)
-				if [ $player_result -gt $opponent_result ]
-				then
-					printf "1"
-				else
-					printf "."
-					printf "\n./$vm -f $map -p1 $p1 -p2 $p2\n" >> $logfile
-					cat filler.trace >> $logfile
-				fi
-			done
+			play $map $opponent $player
 			printf "\n"
 		done
 	done
